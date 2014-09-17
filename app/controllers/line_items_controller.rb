0cee01ee18ece5cee9546f24197833e91aa7cfd6
tusimbe <tusimbe@gmail.com>
2014-09-17 13:13:56 +0800

    Adding cart functions

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -7,11 +7,48 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html {}
-        format.js   { @current_item = @line_item }
+        format.js   {}
       else
         format.html {}
         format.js   {}
       end
+    end
+  end
+
+  def update
+    @cart = current_cart
+    @line_item = @cart.line_items.find(params[:id])
+    oper = params[:oper]
+
+    if oper == "plus"
+      @line_item.quantity += 1
+      @line_item.save
+    elsif oper == "minus"
+      if @line_item.quantity == 1
+        @line_item.destroy
+      else
+        @line_item.quantity -= 1
+        @line_item.save
+      end
+    else
+      # do noting
+    end
+
+    respond_to do |format|
+      format.html {}
+      format.js   {}
+    end
+  end
+
+  def destroy
+    @cart = current_cart
+
+    @line_item = @cart.line_items.find(params[:id])
+    @line_item.destroy
+
+    respond_to do |format|
+      format.html {}
+      format.js   {}
     end
   end
 end
