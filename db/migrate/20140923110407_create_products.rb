25e969a8fe0e82626ea72d1df00b6f24ee5b7ee3
tusimbe <tusimbe@gmail.com>
2014-09-23 19:31:23 +0800

    fix confict rails_admin and will_paginate

---
+++ db/migrate/20140923110407_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string    :title
+      t.text      :description
+      t.decimal   :price
+      t.integer   :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919130639) do
+ActiveRecord::Schema.define(version: 20140923110407) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -56,8 +56,14 @@ ActiveRecord::Schema.define(version: 20140919130639) do
     t.datetime "updated_at"
   end
 
-# Could not dump table "products" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.integer  "status",      default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
